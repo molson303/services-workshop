37d024fdc553d409ed21fee6f88d2958894bbb04
MIchael <michael.olson756@gmail.com>
2016-08-17 13:12:20 -0600

    done

--- controllers/messages_controller.js
+++ controllers/messages_controller.js
@@ -1,4 +1,6 @@
-app.controller('MessagesController', function ($scope) {
+app.controller('MessagesController', function ($scope, MessagesService) {
   $scope.vw = {};
   $scope.vw.success = "Success!!"
+   $scope.vw.allMessages = MessagesService.all
+
 })
--- controllers/post_message_controller.js
+++ controllers/post_message_controller.js
@@ -1,4 +1,13 @@
-app.controller('PostMessageController', function ($scope) {
+app.controller('PostMessageController', function ($scope, MessagesService, $location) {
   $scope.vw = {};
   $scope.vw.success = "Success!!"
+
+  $scope.vw.post_message = function(message){
+    var messageObject = {}
+    messageObject.text = $scope.message
+    MessagesService.add(messageObject)
+    $location.url("/");
+
+}
+
 })
